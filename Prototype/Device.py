d52d67a46c19fb0c46dd7442eda8ad6253f7316b
XRDamage <xavier18.ramage@gmail.com>
2024-05-24 15:43:44 +0200

    Finalized everything but the test program

--- Prototype/Device.py
+++ Prototype/Device.py
@@ -13,12 +13,17 @@ class Device:
 
 
     def sendData(self, data, otherDevice):
-        # Encrypting and hashing data for transfer
-        encData = Cryptography.asymmetricEncryption(data, self.negotiateKeyLevel(otherDevice))
-        hashValue = Cryptography.hash(encData, f"private{self.securityLevel}.pem")
-        # Sending data
-        packet = DataPacket(encData, hashValue)
-        otherDevice.receiveData(packet, self)
+        try:
+            batches = self.splitPackets(data, 100)
+            for batch in batches:
+                # Encrypting and hashing data for transfer
+                encData = Cryptography.asymmetricEncryption(batch, self.negotiateKeyLevel(otherDevice))
+                hashValue = Cryptography.hash(encData, f"private{self.securityLevel}.pem")
+                # Sending data
+                packet = DataPacket(encData, hashValue)
+                otherDevice.receiveData(packet, self)
+        except Exception as e:
+            print(f"Error during encryption or sending: {e}")
         
         # For the purpose of the project, the sending and recieving 
         # will be done by calling the different class methods.
@@ -27,24 +32,40 @@ class Device:
 
 
     def receiveData(self, dataPacket, otherDevice):
-        # Decrypting data that was recieveds
-        data = Cryptography.asymmetricDecryption(dataPacket.encryptedData, f"private{self.securityLevel}.pem")
-        # Validating signature
-        if not dataPacket.checkSignature(otherDevice.encryptionKey):
-            print("Signiture was not verified!")
-        else:
-            print(f"Verified and recieved from {otherDevice.name}")
+        try:
+            if dataPacket.checkSignature(otherDevice.encryptionKey):
+                # Decrypting data that was recieveds
+                data = Cryptography.asymmetricDecryption(dataPacket.encryptedData, f"private{self.securityLevel}.pem")
+                if self.completeData is None:
+                    self.completeData = []
+                self.completeData.append(data.decode())
+            else:
+                print("Signiture not validated")
+        except Exception as e:
+            print(f"Error during data decryption: {e}")
         
-        print(data)
         # For the purpose of the project, the sending and recieving 
         # will be done by calling the different class methods.
         # In a network , the sending and recieving will be done
-        # over network ports 
+        # over network ports
+
+    def viewRecievedData(self):
+        data =  self.reconstructPackets(self.completeData)
+        print(data)
+        self.completeData = None
 
 
-    def splitPackets(self, data):
-        print()
+    def splitPackets(self, data, batchSize=100):
+        # splitting the data into smaller batches
+        if isinstance(data, bytes):
+            codeData = data
+        elif isinstance(data, str):
+            codeData = data.encode('utf-8')
+        else:
+            raise ValueError("Data not string or byte")
+        return [codeData[i:i + batchSize] for i in range(0, len(codeData), batchSize)]
 
 
-    def reconstructPackets(self, data):
-        print()
+    def reconstructPackets(self, batches):
+        # Reconstructing the batches into original data
+        return ''.join(batches)
--- Prototype/main.py
+++ Prototype/main.py
@@ -3,9 +3,12 @@ from DataPacket import *
 from Cryptography import *
 from KeyGenerator import *
 
+
 device1 = Device("Xavier", 1, "public1.pem")
 device2 = Device("Cayden", 2, "public2.pem")
 
-data = "Hello World!"
+with open("Text.txt", "rb") as f:
+    data = f.read()
 
 device1.sendData(data, device2)
+device2.viewRecievedData()
